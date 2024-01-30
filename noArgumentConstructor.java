dbcd4544b3064164102d79f4c539eb40965b544e
Md. Atik hasan <109493827+atikhasan007@users.noreply.github.com>
2024-01-31 02:58:33 +0600

    Create noArgumentConstructor.java

---
+++ noArgumentConstructor.java
@@ -0,0 +1,13 @@
+public class noArgumentConstructor{
+    
+    public noArgumentConstructor() {
+        System.out.println("No-arguments constructor called.");
+    }
+
+    public static void main(String[] args) {
+        
+        noArgumentConstructor obj = new noArgumentConstructor();
+        
+        
+    }
+}
